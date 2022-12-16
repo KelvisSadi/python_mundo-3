8bdad9b2c8393bed74963f75f0561a32d9fc2288
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-16 14:44:59 -0300

    Finalizando o ex076.py

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,9 +1,13 @@
 produtos = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Comapasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
 produto = 0
+print('-'*50)
+print(f'{"LISTAGEM DE PREÇOS":^50}')
+print('-'*50)
 while True:
     print(f'{produtos[produto]:.<40} R$ ', end='')
     produto += 1
-    print(f'{produtos[produto]:>4.2f}')
+    print(f'{produtos[produto]:>6.2f}')
     produto += 1
     if produto == len(produtos):
         break
+print('-'*50)
