1b8d003f2663b1b89f46bab55a4d7574781baf86
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-16 16:31:15 -0300

    Adicionando continuar

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -1,5 +1,15 @@
-numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-num = int(input('Digite um número inteiro entre 0 e 20:'))
-while num < 0 or num > 20:
-    num = int(input('Tente novamente. Digite um número entre 0 e 20:'))
-print(f'Você digitou o número {numeros[num]}')
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+           'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número inteiro entre 0 e 20:'))
+    if num < 0 or num > 20:
+        print('Tente novamente.', end=' ')
+    else:
+        print(f'Você digitou o número {numeros[num]}')
+        continuar = ''
+        while continuar != 'S':
+            continuar = str(input('Quer continuar [S/N]')).upper().strip()[0]
+            if continuar == 'N':
+                break
+    if continuar == 'N':
+        break
