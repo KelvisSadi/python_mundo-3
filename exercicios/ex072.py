7c080a705dd7efd6258ad618acdee611ee6b581c
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-16 09:12:35 -0300

    att string do print final

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -2,4 +2,4 @@ numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oi
 num = int(input('Digite um número inteiro entre 0 e 20:'))
 while num < 0 or num > 20:
     num = int(input('Tente novamente. Digite um número entre 0 e 20:'))
-print(numeros[num])
+print(f'Você digitou o número {numeros[num]}')
