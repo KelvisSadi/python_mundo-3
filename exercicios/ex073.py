f3b61484afa27c7d33b32061cc5d115935189cb5
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-16 09:39:25 -0300

    Criando o ex073.py

---
+++ exercicios/ex073.py
@@ -0,0 +1,8 @@
+times_brasileirão = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthias', 'Flamengo', 'Athletico-PR', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goias', 'Bragantino', 'Cortiba', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avai', 'Juventude')
+print(f'Primeiros cinco times do Brasileirão: {times_brasileirão[:5]}')
+print('-'*30)
+print(f'Últimos quatro times do Brasileirão: {times_brasileirão[15:]}')
+print('-'*30)
+print(f'Times do Brasileirão em ordem alfabética: {sorted(times_brasileirão)}')
+print('-'*30)
+print(f'O Internacional está  na {times_brasileirão.index("Internacional") + 1}ª posição')
