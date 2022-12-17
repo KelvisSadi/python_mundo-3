409d03a19454fc3331e6b781360a6ee776180036
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-17 11:11:10 -0300

    utilizando input dentro da tupla

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -1,10 +1,8 @@
-a = int(input('Digite um número:'))
-b = int(input('Digite um número:'))
-c = int(input('Digite um número:'))
-d = int(input('Digite um número:'))
-tupla = (a, b, c, d)
-print('A tupla não contem nenhum número 9')if 9 not in tupla else print(f'O número nove apareceu {tupla.count(9)} vezes')
-print('A tupla não contém nenhum número 3')if 3 not in tupla else print(f'O primeiro número 3 apareceu na posição {tupla.index(3) + 1}')
+tupla = (int(input('Digite o 1º número:')), int(input('Digite o 2º número:')),
+         int(input('Digite o 3º número:')), int(input('Digite o 4º número:')))
+print(f'A tupla contém: {tupla}')
+print('A tupla não contem número 9')if 9 not in tupla else print(f'O número nove apareceu {tupla.count(9)} vezes')
+print('A tupla não contém número 3')if 3 not in tupla else print(f'O primeiro número 3 apareceu na posição {tupla.index(3) + 1}')
 print('Os números pares são: ', end='')
 for num in tupla:
     if num % 2 == 0:
