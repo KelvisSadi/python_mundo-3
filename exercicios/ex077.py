f9f6e375f82cf1da81b2b44180cf8970673019c0
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-18 10:07:09 -0300

    Adicionando o jeito do Guanabara.

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -1,6 +1,9 @@
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
             'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
 vogais = ('a', 'e', 'i', 'o', 'u')
+print('-'*40)
+print('Contando vogais em Tupla')
+print('-'*40)
 for palavra in palavras:  # para cada palavra em palvras faça:
     print(f'{palavra}:', end=' ')  # printa a palavra atual no for
     for vogal in vogais:  # para cada vogal em vogais faça:
@@ -9,3 +12,12 @@ for palavra in palavras:  # para cada palavra em palvras faça:
             print(f'{vogal * qtd}', end='')  # printa a vogal * a qtd de vez que tem ela
     print('')
     qtd = 0  # zera a qtd para a verificação da próxima palavra
+print('-'*40)
+print('Jeito Guanabara')
+print('-'*40)
+for p in palavras:
+    print(f'\nNa palavra {p} temos:', end=' ')
+    for letra in p:
+        if letra in 'aeiou':
+            print(letra, end=' ')
+
