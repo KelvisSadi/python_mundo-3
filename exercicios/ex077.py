2a779ce41888974bc7ab70d6d1878c33d57b79bb
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-18 09:50:54 -0300

    Criado uma lógica melhor e menor com o for, mais simples e fácil de entender

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -1,19 +1,11 @@
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
             'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
-contador = 0
 vogais = ('a', 'e', 'i', 'o', 'u')
-contador_vogais = 0
-while True:  # while n1
-    print(f'Na palavra {palavras[contador]} temos: ', end='')
-    while True:  # while n2
-        qtd_vogal = palavras[contador].count(vogais[contador_vogais])  # conta quantos vagais 'x' tem na palavra
-        if qtd_vogal > 0:  # se tiver mais que zero faça
-            print(vogais[contador_vogais] * qtd_vogal, end='')  # printa vogal[x] * qtd que ela aparece na palavra
-        contador_vogais += 1  # contador_vogais recebe mais um e agora vai verificar a proxima vogal na mesma frase
-        if contador_vogais == len(vogais):  # se todas as vogais forem verificadas termina o while
-            break  # break n2
-    print('\n')
-    contador += 1  # contador recebe mais um, assim mudando a palavra
-    contador_vogais = 0  # contador recebe zero para verificar cada vogal na proxima palavra
-    if contador == len(palavras):
-        break  # break n1
+for palavra in palavras:  # para cada palavra em palvras faça:
+    print(f'{palavra}:', end=' ')  # printa a palavra atual no for
+    for vogal in vogais:  # para cada vogal em vogais faça:
+        qtd = palavra.count(vogal)  # conta a quantidade da vogal atual dentro da palavra
+        if vogal in palavra:  # se tem a vogal atual na palavra faça:
+            print(f'{vogal * qtd}', end='')  # printa a vogal * a qtd de vez que tem ela
+    print('')
+    qtd = 0  # zera a qtd para a verificação da próxima palavra
