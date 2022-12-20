5ce34cca3beab8c12a21e0c5dac999b1eaf0cd12
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-20 15:22:44 -0300

    Adicionando comentários

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,18 +1,17 @@
 valores = list()
-while True:
+while True:  # while infinito
     num = int(input('Digite um valor:'))
-    if num in valores:
+    if num in valores:  # if {num} estiver em valores faça:
         print('Valor duplicado, NÃO adicionado!')
-    else:
+    else:  # se não faça
         valores.append(num)
         print('Valor ADICIONADO com sucesso...')
     print(f'{"---":^30}')
     continuar = ''
-    while continuar != 'S' and continuar != 'N':
-        continuar = str(input('Deseja Continuar [S/N]')).strip().upper()[0]
+    while continuar != 'S' and continuar != 'N':  # enquanto continuar diferente de 'S' e 'N'OBS:acaba se for 'S' ou 'N'
+        continuar = str(input('Deseja Continuar [S/N]: ')).strip().upper()[0]
         print(f'{"---":^30}')
-    if continuar == 'N':
+    if continuar == 'N':  # se {continuar} igual a 'N'
         break
-valores.sort()
-print(f'Você digitou os valores {valores}')
+print(f'Você digitou os valores: {sorted(valores)}')  # pode usar também ex:sorted(valores, reverse=True)
 print(f'{" Programa encerrado ":-^30}')
