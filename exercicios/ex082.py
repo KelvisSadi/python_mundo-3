d7c44e451b11809691ec65c3a70b5f14669ad980
KelvisSadi <kelvis.sadi@gmail.com>
2022-12-21 08:43:55 -0300

    Adicionando comentários

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -1,17 +1,17 @@
 valores = list()
 valores_imp = list()
 valores_par = list()
-while True:
+while True:  # while infinito n1
     num = int(input('Digite um número:'))
     valores.append(num)
     continuar = ''
-    while continuar != 'S' and continuar != 'N':
+    while continuar != 'S' and continuar != 'N':  # while {S} e {N} diferente de {continuar}
         continuar = str(input('Deseja continuar adicionado números? [S/N]')).strip().upper()[0]
-    if continuar == 'N':
-        break
-for item in valores:
-    if item % 2 == 0:
+    if continuar == 'N':  # se {continuar} igual {N}
+        break  # break n1
+for item in valores:  # laço para cada item em valores
+    if item % 2 == 0:  # se o resto da divisão de item por 2 for igual a zero, quer dizer que é PAR
         valores_par.append(item)
-    else:
+    else:  # se não, só pode ser ímpar
         valores_imp.append(item)
 print(f'Valores: {valores} \nValores pares: {valores_par} \nValores ímpares: {valores_imp}')
